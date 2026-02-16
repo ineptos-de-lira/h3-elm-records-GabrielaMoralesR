851475b7e5c7a176b20efe8719116ff6c36d69af
GabrielaMoralesR <up240055@alumnos.upa.edu.mx>
2026-02-15 21:49:33 -0600

    Records

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,124 @@
 module Helper exposing (..)
+
+import Html
+
+
+
+--Records de lenguaje de programación
+
+
+languages : List { name : String, releaseYear : Int, currentVersion : String }
+languages =
+    [ { name = "elm"
+      , releaseYear = 2012
+      , currentVersion = "0.19.1"
+      }
+    , { name = "javascript"
+      , releaseYear = 1995
+      , currentVersion = "ECMAScript 2025"
+      }
+    ]
+
+
+languageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
+languageNames list =
+    List.map .name list
+
+
+
+--Records de persona
+
+
+users : List { name : String, uType : String }
+users =
+    [ { name = "Roberto"
+      , uType = "Student"
+      }
+    , { name = "Mitsiu"
+      , uType = "Professor"
+      }
+    ]
+
+
+onlyStudents : List { name : String, uType : String } -> List String
+onlyStudents list =
+    let
+        checkStudent : { name : String, uType : String } -> String
+        checkStudent user =
+            if user.uType == "Student" then
+                user.name
+
+            else
+                ""
+    in
+    List.map checkStudent list
+
+
+
+--Para crear videojuegos tipo .--..-
+
+
+type alias VideoGames =
+    { title : String, release : Int, available : Bool, downloads : Int, genres : List String }
+
+
+games : List VideoGames
+games =
+    [ { title = "Stardew Valley"
+      , release = 1956
+      , available = True
+      , downloads = 1655155
+      , genres = [ "Action", "Granja", "Cultivar", "Aldeanos" ]
+      }
+    , { title = "Roblox"
+      , release = 2005
+      , available = False
+      , downloads = 845125
+      , genres = [ "Action", "Online", "Multijuegos", "Avatar" ]
+      }
+    ]
+
+
+getVideogamesGenres : List VideoGames -> List (List String)
+getVideogamesGenres list =
+    List.map .genres list
+
+
+
+--HTML
+
+
+type alias Computer =
+    { ram : String, model : String, brand : String, screenSize : String }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "12GB"
+    , model = "IdeaPad 3 "
+    , brand = "Lenovo"
+    , screenSize = "15 pulgadas"
+    }
+
+
+main : Html.Html msg
+main =
+    Html.div []
+        [ Html.h1 [] [ Html.text "My laptop" ]
+        , Html.div []
+            [ Html.ul []
+                [ Html.li []
+                    [ Html.text ("Ram: " ++ myLaptop.ram)
+                    ]
+                , Html.li []
+                    [ Html.text ("Modelo: " ++ myLaptop.model)
+                    ]
+                , Html.li []
+                    [ Html.text ("Marca: " ++ myLaptop.brand)
+                    ]
+                , Html.li []
+                    [ Html.text ("Pulgadas: " ++ myLaptop.screenSize)
+                    ]
+                ]
+            ]
+        ]
